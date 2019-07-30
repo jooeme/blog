bd4669a77cfaaaa7fc324d27bb1fe882dcb89868
jooe <jooe@Jooe-MacBook-Pro.local>
2019-07-30 19:19:40 +0800

    update github link

--- src/components/header.js
+++ src/components/header.js
@@ -78,7 +78,7 @@ const Header = ({ siteTitle }) => (
       <p>
         <HomeLink to="/">{siteTitle}</HomeLink>
         <NavLink to="/blog">Blog</NavLink>
-        <GitHubLink href="https://github.com/jooeme">GitHub</GitHubLink>
+        <GitHubLink href="https://github.com/jooeme/blog">GitHub</GitHubLink>
       </p>
     </Content>
   </SiteHeader>
